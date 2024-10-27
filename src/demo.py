3d00bd57b393e6f6dd554cff930af934515a92c9
Chris Rauch <70023998+cr625@users.noreply.github.com>
2024-10-27 14:20:42 -0400

    update ontology references

--- src/demo.py
+++ src/demo.py
@@ -4,11 +4,13 @@ from owlready2 import *
 def main():
     onto_path.append("data/")
 
-    onto = get_ontology("data/bacteria.owl").load()
+    # onto = get_ontology("data/bacteria.owl").load()
     cco = get_ontology("data/MergedAllCoreOntology.rdf").load()
     bfo = get_ontology("data/bfo-core").load()
     cco.base_iri = "http://www.ontologyrepository.com/CommonCoreOntologies/"
+    ogms = get_ontology("data/ogms.owl").load()
     obo = get_namespace("http://purl.obolibrary.org/obo/")
+    tccc = get_ontology("data/TCCCApplicationOntology.rdf").load()
 
     # for c in cco.classes():
     #    print(c)
@@ -18,10 +20,12 @@ def main():
     print(agent.iri)
     print(agent.is_a)
 
-    agent_parent = cco.search_one(is_a=obo.BFO_0000040)
-    print(agent_parent)
-    print(obo.BFO_0000040.label)
-    print(agent_parent.label)
+    print("Classes in the ontology:")
+    for cls in tccc.classes():
+        print(cls)
+
+#    agent_parent = cco.search_one(is_a=obo.BFO_0000040)
+#    print(agent_parent.label)
 
 
 if __name__ == "__main__":
